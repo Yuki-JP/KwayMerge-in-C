4a8ee67cb4fb5df9322cea56d71cf738cb819a75
PabloSSena <pablosilvasena@gmail.com>
2021-11-30 17:03:15 -0300

    quickSort quase la

--- main.c
+++ main.c
@@ -23,34 +23,34 @@ void s_archive(char *archive, ITEM_VENDA *array, int n_registros){
     }
 }
 
-void switchPlaces(int *a, int small, int big,ITEM_VENDA* item){  
+void switchPlaces(int *a, int small, int big,ITEM_VENDA item){  
     ITEM_VENDA receiver;
 
     int aux = a[big];
     a[big] =  a[small];
     a[small] = aux;
 
-    receiver.id = item[big].id;
-    receiver.id_venda = item[big].id_venda;
-    receiver.data = item[big].data;
-    receiver.desconto = item[big].desconto;
-    strcpy(receiver.obs,item[big].obs);
+    // receiver.id = item[big].id;
+    // receiver.id_venda = item[big].id_venda;
+    // receiver.data = item[big].data;
+    // receiver.desconto = item[big].desconto;
+    // strcpy(receiver.obs,item[big].obs);
 
-    item[big].id = item[small].id;
-    item[big].id_venda = item[small].id_venda;
-    item[big].data = item[small].data;
-    item[big].desconto = item[small].desconto;
-    strcpy(item[big].obs,item[small].obs); 
+    // item[big].id = item[small].id;
+    // item[big].id_venda = item[small].id_venda;
+    // item[big].data = item[small].data;
+    // item[big].desconto = item[small].desconto;
+    // strcpy(item[big].obs,item[small].obs); 
 
-    item[small].id = item[big].id;
-    item[small].id_venda = item[big].id_venda;
-    item[small].data = item[big].data;
-    item[small].desconto = item[big].desconto;
-    strcpy(item[small].obs,item[big].obs);
+    // item[small].id = item[big].id;
+    // item[small].id_venda = item[big].id_venda;
+    // item[small].data = item[big].data;
+    // item[small].desconto = item[big].desconto;
+    // strcpy(item[small].obs,item[big].obs);
 
 };
 
-int partition(int *a, int first, int last, ITEM_VENDA* item){
+int partition(int *a, int first, int last, ITEM_VENDA item){
     int pivot;
     int yellowLine, purpleLine;
 
@@ -67,7 +67,7 @@ int partition(int *a, int first, int last, ITEM_VENDA* item){
 
     return yellowLine + 1;
 }
-void quickSort(int *array,int initial, int final, ITEM_VENDA* item){
+void quickSort(int *array,int initial, int final, ITEM_VENDA item){
     int pointer;
 
     if( initial < final ) {
@@ -80,9 +80,9 @@ void quickSort(int *array,int initial, int final, ITEM_VENDA* item){
 void create(char *archive){
     ITEM_VENDA receiver,auxArray[LIMIT];
     int archiveNumber = 0, numberOfBuffer = 0,i = 0,readed;
-    ITEM_VENDA *toSave = malloc(LIMIT * sizeof(ITEM_VENDA));// Lembrar que se guardar mais coisa aqui do que ele aguenta vai dar pau
+    ITEM_VENDA toSave[LIMIT];// Lembrar que se guardar mais coisa aqui do que ele aguenta vai dar pau
     char *nameDefiner[30]; // O pica ta
-    uint32_t *arrayToquick[LIMIT];
+    uint32_t arrayToquick[LIMIT];
 
     FILE *archiveToRead = fopen(archive,"r");
     if(archiveToRead == NULL){
@@ -94,28 +94,35 @@ void create(char *archive){
         numberOfBuffer++;
         if(numberOfBuffer == 10){
             archiveNumber++;
-            sprintf(nameDefiner,"Temp%d.txt",archiveNumber);
+            sprintf(nameDefiner,"testeTemp%d.txt",archiveNumber);
 
-            for(int y = 0; i < LIMIT;y++){
+            for(int y = 0; y < LIMIT;y++){
                 arrayToquick[y] = toSave[y].id;
             }
 
 
-            quickSort(arrayToquick,0,LIMIT,toSave);
+            quickSort(arrayToquick,0,LIMIT,toSave[LIMIT]);
             s_archive(nameDefiner,&toSave,LIMIT);
             numberOfBuffer = 0;
         }
         //printf("Testezinho = %d\n",toSave[i].id);
         i++;
     }
-    fclose(archiveToRead);
+    //fclose(archiveToRead);
 }
 
 
 int main(){
     ITEM_VENDA tester[301];
+    ITEM_VENDA *toSave = malloc(LIMIT * sizeof(ITEM_VENDA));// Lembrar que se guardar mais coisa aqui do que ele aguenta vai dar pau
+    int numberOfBuffer = 0;
+    FILE *archiveToRead = fopen("testeTemp1.txt","r");
     create("teste.dat");
 
+    for(int h = 0; h < 10;h++){
+        int readed = fread(&toSave[numberOfBuffer],sizeof(ITEM_VENDA),1,archiveToRead);
+        printf("id = %d\n", toSave[h].id);
+    }
     // for(int i = 0; i < 100; i++){
     //     int readed = fread(&tester[i],sizeof(ITEM_VENDA),1,"Temp1.txt");
     //     printf("tester[i].id = %d\n",i,tester[i].id);
